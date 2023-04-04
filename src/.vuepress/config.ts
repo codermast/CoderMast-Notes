1d5359de3ec2a7ffbc64782fee6843307870eebb
codermast <codermast@qq.com>
2023-04-04 09:12:09 +0800

    增加主题颜色自选器

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -12,8 +13,14 @@ export default defineUserConfig({
     },
   },
 
-  theme,
-
+  theme: hopeTheme({
+    themeColor: {
+      blue: "#2196f3",
+      red: "#f26d6d",
+      green: "#3eaf7c",
+      orange: "#fb9b5f",
+    },
+  }),
   // Enable it with pwa
   // shouldPrefetch: false,
 });
