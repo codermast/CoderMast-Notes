75cab161280a2d11183e9943bfcd36e8893a0353
codermast <codermast@qq.com>
2023-04-04 09:24:17 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,14 +4,6 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: "/",
 
-  locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
-    },
-  },
-
   theme,
 
   // Enable it with pwa
