a4f00292af8dce4c907454076458761ace855de2
codermast <codermast@qq.com>
2023-04-04 11:06:39 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,29 +1,13 @@
 import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro"
 import theme from "./theme.js";
-import { git } from "vuepress-theme-hope";
+import { git, hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   base: "/",
-  plugins: [
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
-  ],
   theme,
+
+});
 
   // Enable it with pwa
   // shouldPrefetch: false,
-});
--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -55,9 +55,4 @@ export const zhNavbar = navbar([
       },
     ],
   },
-  {
-    text: "V2 文档",
-    icon: "note",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
 ]);
