dbbb270d02aeaa9afd9b65a80ac84d6160c26878
codermast <codermast@qq.com>
2023-06-18 23:43:42 +0800

    Redis五种数据类型的底层结构关系

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -67,6 +67,8 @@ export const pagesComponents = {
   "v-13f32783": defineAsyncComponent(() => import(/* webpackChunkName: "v-13f32783" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/mysql/transaction.html.vue")),
   // path: /database/redis/base-commind.html
   "v-199a53b3": defineAsyncComponent(() => import(/* webpackChunkName: "v-199a53b3" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/redis/base-commind.html.vue")),
+  // path: /database/redis/base-datatype-implement.html
+  "v-350dca32": defineAsyncComponent(() => import(/* webpackChunkName: "v-350dca32" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/redis/base-datatype-implement.html.vue")),
   // path: /database/redis/cache-issues.html
   "v-5d458936": defineAsyncComponent(() => import(/* webpackChunkName: "v-5d458936" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/redis/cache-issues.html.vue")),
   // path: /database/redis/five-base-datatype.html
@@ -97,6 +99,16 @@ export const pagesComponents = {
   "v-a746b360": defineAsyncComponent(() => import(/* webpackChunkName: "v-a746b360" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/redis/spring-data-redis.html.vue")),
   // path: /database/redis/three-special-datatype.html
   "v-7a4ca203": defineAsyncComponent(() => import(/* webpackChunkName: "v-7a4ca203" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/database/redis/three-special-datatype.html.vue")),
+  // path: /computer/cn/
+  "v-9a72c4ae": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72c4ae" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/index.html.vue")),
+  // path: /computer/cn/cn-tcp-3-4.html
+  "v-1be8c5c4": defineAsyncComponent(() => import(/* webpackChunkName: "v-1be8c5c4" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/cn-tcp-3-4.html.vue")),
+  // path: /computer/co/
+  "v-9a72c470": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72c470" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/co/index.html.vue")),
+  // path: /computer/ds/
+  "v-9a72bbf6": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72bbf6" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/index.html.vue")),
+  // path: /computer/os/
+  "v-9a726960": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a726960" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/os/index.html.vue")),
   // path: /problems/leetcode/leetcode15.html
   "v-a94b64b0": defineAsyncComponent(() => import(/* webpackChunkName: "v-a94b64b0" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/problems/leetcode/leetcode15.html.vue")),
   // path: /problems/leetcode/leetcode1646.html
@@ -115,20 +127,10 @@ export const pagesComponents = {
   "v-04c0516d": defineAsyncComponent(() => import(/* webpackChunkName: "v-04c0516d" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/problems/leetcode/leetcode746.html.vue")),
   // path: /problems/leetcode/
   "v-56c36d18": defineAsyncComponent(() => import(/* webpackChunkName: "v-56c36d18" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/problems/leetcode/index.html.vue")),
-  // path: /computer/cn/
-  "v-9a72c4ae": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72c4ae" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/index.html.vue")),
-  // path: /computer/cn/cn-tcp-3-4.html
-  "v-1be8c5c4": defineAsyncComponent(() => import(/* webpackChunkName: "v-1be8c5c4" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/cn-tcp-3-4.html.vue")),
-  // path: /computer/co/
-  "v-9a72c470": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72c470" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/co/index.html.vue")),
-  // path: /computer/ds/
-  "v-9a72bbf6": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a72bbf6" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/index.html.vue")),
   // path: /tools/stata/
   "v-594b2522": defineAsyncComponent(() => import(/* webpackChunkName: "v-594b2522" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/tools/stata/index.html.vue")),
   // path: /tools/stata/stata-commom-instruct.html
   "v-5e01a32b": defineAsyncComponent(() => import(/* webpackChunkName: "v-5e01a32b" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/tools/stata/stata-commom-instruct.html.vue")),
-  // path: /computer/os/
-  "v-9a726960": defineAsyncComponent(() => import(/* webpackChunkName: "v-9a726960" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/os/index.html.vue")),
   // path: /computer/cn/application-layer/
   "v-4bbcf4f8": defineAsyncComponent(() => import(/* webpackChunkName: "v-4bbcf4f8" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/application-layer/index.html.vue")),
   // path: /computer/cn/cn-architecture/
@@ -137,34 +139,34 @@ export const pagesComponents = {
   "v-77d179d8": defineAsyncComponent(() => import(/* webpackChunkName: "v-77d179d8" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/cn-architecture/cn-classification.html.vue")),
   // path: /computer/cn/cn-architecture/cn-notion-composition-function.html
   "v-33201e36": defineAsyncComponent(() => import(/* webpackChunkName: "v-33201e36" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/cn-architecture/cn-notion-composition-function.html.vue")),
+  // path: /computer/cn/data-link-layer/
+  "v-3d008037": defineAsyncComponent(() => import(/* webpackChunkName: "v-3d008037" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/data-link-layer/index.html.vue")),
   // path: /computer/cn/netwark-layer/
   "v-27d387b4": defineAsyncComponent(() => import(/* webpackChunkName: "v-27d387b4" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/netwark-layer/index.html.vue")),
   // path: /computer/cn/physical-layer/
   "v-41c16c2a": defineAsyncComponent(() => import(/* webpackChunkName: "v-41c16c2a" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/physical-layer/index.html.vue")),
-  // path: /computer/cn/data-link-layer/
-  "v-3d008037": defineAsyncComponent(() => import(/* webpackChunkName: "v-3d008037" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/data-link-layer/index.html.vue")),
   // path: /computer/cn/transaction-layer/
   "v-d90a0794": defineAsyncComponent(() => import(/* webpackChunkName: "v-d90a0794" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/cn/transaction-layer/index.html.vue")),
   // path: /computer/ds/graph/graph.html
   "v-27c0ebb1": defineAsyncComponent(() => import(/* webpackChunkName: "v-27c0ebb1" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/graph/graph.html.vue")),
   // path: /computer/ds/graph/
   "v-767bb2e6": defineAsyncComponent(() => import(/* webpackChunkName: "v-767bb2e6" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/graph/index.html.vue")),
-  // path: /computer/ds/introduction/complexity.html
-  "v-62055baa": defineAsyncComponent(() => import(/* webpackChunkName: "v-62055baa" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/introduction/complexity.html.vue")),
-  // path: /computer/ds/introduction/
-  "v-5eac3970": defineAsyncComponent(() => import(/* webpackChunkName: "v-5eac3970" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/introduction/index.html.vue")),
   // path: /computer/ds/linear-list/linear-list.html
   "v-b8ba70fe": defineAsyncComponent(() => import(/* webpackChunkName: "v-b8ba70fe" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/linear-list/linear-list.html.vue")),
   // path: /computer/ds/linear-list/
   "v-5186192e": defineAsyncComponent(() => import(/* webpackChunkName: "v-5186192e" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/linear-list/index.html.vue")),
-  // path: /computer/ds/search/
-  "v-84d1643c": defineAsyncComponent(() => import(/* webpackChunkName: "v-84d1643c" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/search/index.html.vue")),
-  // path: /computer/ds/search/search.html
-  "v-768295e2": defineAsyncComponent(() => import(/* webpackChunkName: "v-768295e2" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/search/search.html.vue")),
+  // path: /computer/ds/introduction/complexity.html
+  "v-62055baa": defineAsyncComponent(() => import(/* webpackChunkName: "v-62055baa" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/introduction/complexity.html.vue")),
+  // path: /computer/ds/introduction/
+  "v-5eac3970": defineAsyncComponent(() => import(/* webpackChunkName: "v-5eac3970" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/introduction/index.html.vue")),
   // path: /computer/ds/sort/
   "v-cdc0d128": defineAsyncComponent(() => import(/* webpackChunkName: "v-cdc0d128" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/sort/index.html.vue")),
   // path: /computer/ds/sort/sort.html
   "v-d1efbfe2": defineAsyncComponent(() => import(/* webpackChunkName: "v-d1efbfe2" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/sort/sort.html.vue")),
+  // path: /computer/ds/search/
+  "v-84d1643c": defineAsyncComponent(() => import(/* webpackChunkName: "v-84d1643c" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/search/index.html.vue")),
+  // path: /computer/ds/search/search.html
+  "v-768295e2": defineAsyncComponent(() => import(/* webpackChunkName: "v-768295e2" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/search/search.html.vue")),
   // path: /computer/ds/stack-queue-array/
   "v-d55a61c8": defineAsyncComponent(() => import(/* webpackChunkName: "v-d55a61c8" */"/Users/codermast/VScodeProjects/CoderMast/docs/.vuepress/.temp/pages/computer/ds/stack-queue-array/index.html.vue")),
   // path: /computer/ds/stack-queue-array/stack-queue-array.html
---
+++ docs/database/redis/base-datatype-implement.md
@@ -0,0 +1,88 @@
+---
+order : 17
+---
+
+# Redis原理 - 五种数据类型的底层结构关系
+
+![](../../../assets/base-datatype-implement/2023-06-18-22-10-10.png)
+
+## 字符串对象String
+
+String 是 Redis 中最常见的数据存储类型。
+
+- 其基本编码方式是 RAW ，基于简单动态字符串（SDS）实现，存储上限为 512 MB。
+
+![](../../../assets/base-datatype-implement/2023-06-18-22-18-01.png)
+
+- 如果存储的 SDS 长度小于 44 字节，则会采用 **EMBSTR** 编码，此时 object head 与 SDS 是一段连续空间。申请内存时只需要调用一次内存分配函数，效率更高。
+
+![](../../../assets/base-datatype-implement/2023-06-18-22-17-44.png)
+
+- 如果存储的字符串是整数型，并且大小在 LONG_MAX 范围内，则会采用INT 编码：直接将数据保存在 RedisObject 的 ptr 指针位置（刚好 8 字节），不再需要 SDS 了。
+
+![](../../../assets/base-datatype-implement/2023-06-18-22-23-54.png)
+
+## 列表对象List
+
+Redis 的 List 结构类似一个双端链表，可以从首、尾操作列表中的元素：
+
+- 在 3.2 版本之前，Redis 采用 ZipList 和LinkedList 来实现 List，当元素数量小于512并且元素大小小于 64 字节时，采用 ZipList 编码，超过则采用 LinkedList 编码
+- 在 3.2 版本之后，Redis 统一采用 QuickList 来实现List
+
+## 哈希对象Hash
+
+哈希对象的键是一个字符串类型，值是一个键值对集合。
+
+哈希对象的编码可以是 ziplist 或者 hashtable；对应的底层实现有两种, 一种是ziplist, 一种是dict。
+
+哈希结构与 Redis 中的 Zset 非常类似：
+
+- 都是键值存储
+- 都需求根据键获取值
+- 键必须唯一
+
+因此 Hash 底层采用的编码与 Zset 基本一致，只需要把排序有关的 SkipList 去掉即可：
+
+- Hash 结构默认采用了 ZipList 编码，用以节省内存。ZipList 中相邻的两个 entry 分别保存 field 和 value
+
+- 当数据量较大时 Hash 结构会转换为 HT 编码，也就是 Dict ，触发条件有两个：
+    1. ZipList 中的元素数量超过了 hash-max-ziplist-entries（默认512）
+    2. ZipList 中的任意 entry 大小超过了 hash-max-ziplist-value（默认64字节）
+## 集合对象Set
+
+Set 是 Redis 中的单列集合，满足下列特点：
+- 不保证有序
+- 保证元素唯一（可以判断元素是否存在）
+- 求交集、并集、差集
+
+Set 是 Redis 中的集合，不一定确保元素有序，可以满足元素唯一、查询效率要求极高。
+- 为了查询效率和唯一性，set 采用 HT 编码（Dict）。Dict 中的 key 用来存储元素，value 统一为 null
+- 当存储的所有数据都是整数，并且元素数量不超过`set-max-intset-entries`时，Set 会采用 IntSet 编码，以节省内存。每一次插入数据时都会判断数据是否为整数。
+## 有序集合对象ZSet
+
+ZSet 也就是 SortedSet ，其中每一个元素都需要指定一个score 值和 member 值。
+
+- 可以根据score 值排序
+- member 必须唯一
+- 可以根据 member 查询分数
+
+因此，ZSet 底层数据结构必须满足键值存储、键必须唯一、可排序这几个需求。
+
+- SkipList：可以排序，并且可以同时存储 score 和 ele 值（member）。可以实现排序功能
+- HT(Dict)：可以键值存储，并且可以根据 key 找 value。可以实现快速访问功能
+
+为了同时实现排序和快速访问的功能，实际上的 ZSet 底层同时使用了两种结构，但由于编码方式只能写一种，这里写的是 OBJ_ENCODING_SKIPLIST
+
+![](../../../assets/base-datatype-implement/2023-06-18-23-18-49.png)
+
+当元素数量不多时，HT 和 SkipList 的优势不明显，而且更耗费内存。因此 ZSet 还采用 ZipList 结构来节省内存，不过需要同时满足连个条件：
+
+1. 元素数量小于 zset_max_ziplist_entries，默认值 128
+2. 每个元素都小于 zset_max_ziplist_value字节，默认值 64
+
+ZipList 本身没有排序功能，也没有键值对的概念，因此需要有 zset 通过编码实现：
+
+- ZipList 是连续内存，因此 score 和 element 是紧挨在一起的两个 entry ，element在前，score 在后。
+- score 越小越接近队首，score 越大越接近队尾，按照 score 值升序排列
+
+![](../../../assets/base-datatype-implement/2023-06-18-23-32-41.png)
